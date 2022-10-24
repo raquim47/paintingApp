66e4f06222e222cb43812564291dd41b89586a9b
jin <raquim47@naver.com>
2022-10-24 12:42:38 +0900

    Add text && img

--- app.js
+++ app.js
@@ -8,6 +8,18 @@ const colorInput = document.querySelector(".color-input");
 const swatches = Array.from(document.querySelectorAll("#swatches td"));
 const swatchPanel = document.querySelector("#swatches");
 const strokePanel = document.querySelector("#stroke");
+
+const fontSizes = document.querySelector("#fontSizes");
+const fontTypes = document.querySelector("#fontTypes");
+const fontWeights = document.querySelector("#fontWeights");
+const textInput = document.querySelector("#text-input");
+const textPanel = document.querySelector("#text");
+const imgPanel = document.querySelector("#img-upload");
+
+const fileInput = document.querySelector("#file");
+const imgReady = document.querySelector("#img-ready");
+const imgSpan = document.querySelector("#img-span");
+
 const CANVAS_SIZE = 600;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
@@ -20,7 +32,10 @@ let isFill = false;
 let isRectangle = false;
 let isCircle = false;
 let isEraser = false;
+let isText = false;
+let isImg = false;
 let colorSaved = 'black';
+let image;
 
 let rectX = 0;
 let rectY = 0;
@@ -47,9 +62,16 @@ const onMouseMove = (e) => {
       ctx.arc(rectX, rectY, cwidth, 0, Math.PI * 2, true);
       ctx.fill();
     }
-  } else if (isEraser){
+  } else if (isEraser) {
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
+  } else if (isImg && image) {
+    const x = e.offsetX;
+    const y = e.offsetY;
+    const width = x - rectX;
+    const height = y - rectY;
+    ctx.moveTo(x, y);
+    ctx.drawImage(image, rectX, rectY, width, height);
   }
   ctx.beginPath();
   ctx.moveTo(e.offsetX, e.offsetY);
@@ -57,7 +79,7 @@ const onMouseMove = (e) => {
 
 const onMouseDown = (e) => {
   isPainting = true;
-  if (isRectangle || isCircle) {
+  if (isRectangle || isCircle || isImg) {
     rectX = e.offsetX;
     rectY = e.offsetY;
   }
@@ -66,19 +88,32 @@ const onMouseDown = (e) => {
 const onMouseUp = () => {
   isPainting = false;
 };
-const onMouseClick = () => {
+const onMouseClick = (e) => {
   if (isFill) {
     ctx.fillRect(0, 0, 800, 800);
-  }
+  } else if (isText) {
+    const text = textInput.value;
+    const textWeight = fontWeights.value;
+    const textSize = fontSizes.value;
+    const textFont = fontTypes.value;
+    ctx.save();
+    ctx.lineWidth = 1;
+    ctx.font = `${textWeight} ${textSize}px ${textFont}`;
+    ctx.fillText(text, e.offsetX, e.offsetY);
+    ctx.restore();
+  } 
 };
 
 const onClickTool = (e) => {
   toolBtns.forEach((btn) => (btn.style.color = "#c2ced6"));
   e.target.style.color = "#94b3ec";
   const data = e.target.parentElement.dataset.text;
-  isBrush = isFill = isRectangle = false;
+  isBrush = isFill = isRectangle = isCircle = isEraser = isText = isImg = false;
+  image = "";
   swatchPanel.style.display = "none";
   strokePanel.style.display = "none";
+  textPanel.style.display = "none";
+  imgPanel.style.display = "none";
   if (data === "브러쉬") {
     isBrush = true;
     canvas.style.cursor = "url(cursors/brush.cur) 0 20, auto";
@@ -102,10 +137,22 @@ const onClickTool = (e) => {
     canvas.style.cursor = "url(cursors/eraser.cur) 0 20, auto";
     strokePanel.style.display = "block";
     ctx.strokeStyle = 'white';
+  } else if (data === "텍스트") {
+    isText = true;
+    canvas.style.cursor = "url(cursors/precision.cur) 0 20, auto";
+    swatchPanel.style.display = "block";
+    textPanel.style.display = "block";
+    textInput.value = "";
+  } else if (data === "이미지") {
+    isImg = true;
+    canvas.style.cursor = "url(cursors/precision.cur) 0 20, auto";
+    imgPanel.style.display = "block";
+    imgSpan.style.display = "none";
+    if (imgReady.querySelector('img')) {
+      imgReady.querySelector('img').remove();
+    }
   }
 };
-
-
 
 const onClickSwatch = (e) => {
   const swatchValue = colorSaved = e.target.dataset.color;
@@ -125,6 +172,19 @@ toolBtns.forEach((btn) => {
   btn.addEventListener("click", (e) => onClickTool(e));
 });
 
+const onFileChange = (e) => {
+  const img = imgReady.querySelector('img');
+  if (img) {
+    img.remove();
+  }
+  const file = e.target.files[0];
+  const url = URL.createObjectURL(file);
+  image = new Image()
+  image.src = url;
+  imgReady.appendChild(image);
+  imgSpan.style.display = "block";
+}
+
 canvas.addEventListener("mousedown", onMouseDown);
 canvas.addEventListener("mouseup", onMouseUp);
 canvas.addEventListener("mouseleave", onMouseUp);
@@ -134,3 +194,4 @@ colorInput.addEventListener("change", onClickColorInput);
 swatches.forEach((swatch) => swatch.addEventListener("click", onClickSwatch));
 
 strokeWidth.addEventListener("change", onChangeStrokeWidth);
+fileInput.addEventListener("change", onFileChange);
